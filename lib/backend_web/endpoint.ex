f8c4dc0f4bdf2f36f1fbdc6bc20fd41e70896d4f
lucaspaszinski <lucaspaszinski@gmail.com>
2020-08-12 19:49:17 -0300

    Added Cors to plug

--- lib/backend_web/endpoint.ex
+++ lib/backend_web/endpoint.ex
@@ -1,6 +1,7 @@
 defmodule BackendWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :backend
 
+
   # The session will be stored in the cookie and signed,
   # this means its contents can be read but not tampered with.
   # Set :encryption_salt if you would also like to encrypt it.
@@ -48,5 +49,6 @@ defmodule BackendWeb.Endpoint do
   plug Plug.MethodOverride
   plug Plug.Head
   plug Plug.Session, @session_options
+  plug CORSPlug 
   plug BackendWeb.Router
 end
